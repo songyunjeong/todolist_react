7ec3c08174c61df032054ae209912d97471b256a
songyunjeong <yungunaa@naver.com>
2023-05-12 23:50:32 +0900

    create

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import Header from "./component/Header";
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
+
+const mockTodo = [
+  {
+    id: 0,
+    idDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getDate(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getDate(),
+  },
+];
+
+function App() {
+  const idRef = useRef(3);
+  const [todo, setTodo] = useState(mockTodo);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
