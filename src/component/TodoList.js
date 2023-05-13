2f9ad50571ea32a3a2c862f88bb01e3aed1eb679
송윤정 <yungunaa@naver.com>
2023-05-13 16:19:10 +0900

    filter

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((item) =>
+          item.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      />
       <div className="list_wrapper">
-        {todo.map((item) => (
+        {getSearchResult().map((item) => (
           <TodoItem key={item.id} {...item} />
         ))}
       </div>
