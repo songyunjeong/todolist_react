af3d8a28e6c1951c83deefaaf07c56e5fe3968a7
송윤정 <yungunaa@naver.com>
2023-05-15 20:18:27 +0900

    최적화 #Memoization useMemo

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
@@ -17,9 +17,26 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
         );
   };
 
+  const analyzeTodo = useMemo(() => {
+    const totalCount = todo.length;
+    const doneCount = todo.filter((item) => item.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todo]);
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo;
+
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>총 개수: {totalCount}</div>
+        <div>완료된 할 일: {doneCount}</div>
+        <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
