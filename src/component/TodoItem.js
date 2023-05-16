407a232e487171695bb41c3ab503c723d011567b
songyunjeong <yungunaa@naver.com>
2023-05-16 21:42:54 +0900

    #Memoization React.memo

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,6 +1,8 @@
+import React from "react";
 import "./TodoItem.css";
 
 const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
+  console.log(`${id} TodoItem 업데이트`);
   const onChangeCheckBox = () => {
     onUpdate(id);
   };
@@ -25,4 +27,4 @@ const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+export default React.memo(TodoItem);
